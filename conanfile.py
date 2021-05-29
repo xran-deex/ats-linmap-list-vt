9493885c41326a09cebff7b11e078be4ca33c5f4
Randy Valis <randy.valis@gmail.com>
2021-05-29 11:02:49 -0400

    WIP

--- conanfile.py
+++ conanfile.py
@@ -3,9 +3,6 @@ from atsconan import ATSConan
 class ATSConan(ATSConan):
     name = "linmap-list-vt"
     version = "0.1"
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": False}
 
     def package_info(self):
         super().package_info()
--- tests/conanfile.py
+++ tests/conanfile.py
@@ -3,9 +3,6 @@ from atsconan import ATSConan
 class ATSConan(ATSConan):
     name = "linmap-list-vt-tests"
     version = "0.1"
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": False}
     requires = "linmap-list-vt/0.1@randy.valis/testing"
     build_requires = "ats-unit-testing/0.1@randy.valis/testing"
 
