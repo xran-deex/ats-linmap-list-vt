3a59e212b073ed7bf7fad426c06ec6080399d93e
Randy Valis <randy.valis@gmail.com>
2022-09-03 11:11:12 -0400

    Refactored cPtr

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from atsconan import ATSConan
 
 class ATSConan(ATSConan):
     name = "linmap-list-vt"
-    version = "0.1"
+    version = "0.2"
 
     def package_info(self):
         super().package_info()
